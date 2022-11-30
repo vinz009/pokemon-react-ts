7ed7f63a1ad049067a81dacb008de261c10a7414
Vincent <vincentamoguis@gmail.com>
2022-11-30 22:27:25 +0800

    chore: change js transpiler from babel to swc

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,11 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        test: /\.m?js$/,
+	exclude: /(node_modules)/,
+	use: {
+		loader: "swc-loader"
+	}
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
